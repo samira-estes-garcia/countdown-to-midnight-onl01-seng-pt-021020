e5a5884b6daae6d280f9bf08618a09deed7e0dbc
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-02-18 22:26:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,10 @@ def countdown(int)
   "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep(int)
+  while int > 0 
+    puts "#{int} SECOND(S)!" sleep (1.second)
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
